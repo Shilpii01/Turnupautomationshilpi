f8d124bf5e89b96cc639b3fea2310f9ea395f24d
Shilpii01 <shilpicharu@gmail.com>
2023-09-23 23:44:30 +1200

    Initial test case for Turnup portal login page added

--- TurnupAutomation/TurnupAutomation/Program.cs
+++ TurnupAutomation/TurnupAutomation/Program.cs
@@ -1,34 +1,32 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
+
 //Open chrome browser
+
+
 IWebDriver cdriver = new ChromeDriver();
 cdriver.Manage().Window.Maximize();
-
-//Lauch turnup portal and navigates to login page
+//Launch turnup portal and navigate to login page 
 cdriver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login");
 
-//Identify username textbox and enter valid username
+//Identify Username textbox and enter valid username
 IWebElement usernametxtbox = cdriver.FindElement(By.Id("UserName"));
 usernametxtbox.SendKeys("hari");
-
 //Identify password textbox and enter valid password
 IWebElement passwordtxtbox = cdriver.FindElement(By.Id("Password"));
 passwordtxtbox.SendKeys("123123");
-
 //Identify login button and click on the button
 IWebElement loginbutton = cdriver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
 loginbutton.Click();
 
-//Check if the user has logged in successfully
-IWebElement hellohari = cdriver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
-if(hellohari.Text == "Hello hari!")
+//Verify if the user has logged in successfully
+IWebElement helloHari = cdriver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+if(helloHari.Text == "Hello hari!")
 {
     Console.WriteLine("User has logged in successfully");
 }
-else
+else 
 {
     Console.WriteLine("Invalid Username/Password");
 }
-
-
