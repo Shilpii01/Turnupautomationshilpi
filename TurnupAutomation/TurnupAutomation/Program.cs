ea80790dd8113b1c5e17f66b60753b47dbfd73fd
Shilpii01 <shilpicharu@gmail.com>
2023-09-27 17:13:02 +1300

    Added the third edit record test case

--- TurnupAutomation/TurnupAutomation/Program.cs
+++ TurnupAutomation/TurnupAutomation/Program.cs
@@ -47,6 +47,7 @@ createnewbutton.Click();
 
 IWebElement tnmoption = cdriver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span"));
 tnmoption.Click();
+Thread.Sleep(1000);
 
 IWebElement tmoption = cdriver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
 tmoption.Click();
@@ -58,6 +59,7 @@ codetxtbox.SendKeys("New record");
 // Enter description in the description textbox
 IWebElement descriptiontxtbox = cdriver.FindElement(By.Id("Description"));
 descriptiontxtbox.SendKeys("data new record");
+
 
 //Enter the price per unit for the new record
 IWebElement pricetxtbox = cdriver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
@@ -83,3 +85,59 @@ else
     Console.WriteLine("no record created");
 }
 
+//Edit the new created record
+IWebElement editbutton = cdriver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+editbutton.Click();
+
+//Edit the type code to material from time
+IWebElement editTypecode = cdriver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+editTypecode.Click();
+
+IWebElement edit1Typecode = cdriver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[1]"));
+edit1Typecode.Click();
+
+//Edit the code textbox 
+IWebElement editcodetextbox = cdriver.FindElement(By.Id("Code"));
+editcodetextbox.Clear();
+editcodetextbox.SendKeys("Edited code");
+
+//Edit the description textbox
+IWebElement editdescriptiontextbox = cdriver.FindElement(By.Id("Description"));
+editdescriptiontextbox.Clear();
+editdescriptiontextbox.SendKeys("Edited description");
+Thread.Sleep(2000);
+
+//Edit price textbox
+IWebElement editpriceoverlappingtag = cdriver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+IWebElement editpricetextbox = cdriver.FindElement(By.Id("Price"));
+editpriceoverlappingtag.Click();
+editpricetextbox.Clear();
+editpriceoverlappingtag.Click();
+
+editpricetextbox.SendKeys("980.76");
+
+//Download the edited record 
+IWebElement downloadbutton = cdriver.FindElement(By.Id("downloadButton"));
+downloadbutton.Click();
+
+//Save the edited record
+IWebElement editsavebutton = cdriver.FindElement(By.Id("SaveButton"));
+editsavebutton.Click();
+
+Thread.Sleep(6000);
+
+//Check if the last record is edited successfully 
+IWebElement endpagebutton = cdriver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+endpagebutton.Click();
+
+IWebElement editedrecord = cdriver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+if (editedrecord.Text == "Edited code")
+{
+    Console.WriteLine("User has been edited successfully");
+}
+else
+{
+    Console.WriteLine("no record edited");
+}
+
+//delete the last created record 
